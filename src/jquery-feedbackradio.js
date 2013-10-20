fb92c9103cd4f69c81b7c8a57fea62de36b7bdee
Ain Tohvri <ain@flashbit.net>
2013-10-21 01:11:59 +0200

    Add basic initialisation, defaults

--- src/jquery-feedbackradio.js
+++ src/jquery-feedbackradio.js
@@ -16,6 +16,12 @@
   var pluginName = 'feedbackradio';
 
   var defaults = {
+    colours: {
+      active: '#ccc',
+      selected: '#ddd',
+      hover: '#eee'
+    },
+    increment: 2
   };
 
   function FeedbackRadio(element, options) {
@@ -23,13 +29,22 @@
     this.options = $.extend({}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
+    this._init();
   }
+  
+  FeedbackRadio.prototype._init = function() {
+    var self = this;
+    $(this.element).each(function() {
+      $(this).hide();
+      // TODO replace radio input with star(s)
+    });
+  };
 
   $.fn[pluginName] = function(options) {
     return this.each(function() {
       if (!$.data(this, 'plugin_' + pluginName)) {
         $.data(this, 'plugin_' + pluginName,
-            new FeedbackRadio(this, options));
+          new FeedbackRadio(this, options));
       }
     });
   };
